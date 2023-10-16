a7645f2db2c8adc32b3575729a63ebb53cccf9a2
SeniboDonPedro <senibodonpedro@gmail.com>
2023-10-16 12:44:22 +0100

    added a ! to wsk.jsx

--- src/containers/Wsk/Wsk.jsx
+++ src/containers/Wsk/Wsk.jsx
@@ -5,7 +5,7 @@ const Wsk = () => {
   return (
     <div className='sk__wsk section__margin' id='wsk'>
       <div className='sk__wsk-feature'>
-        <Feature title="Why Sure Kicks?" text="We're on a mission to provide the most stylish and comfortable shoes to our valued customers, all while ensuring the highest quality and unbeatable customer service. We believe that the right pair of shoes can transform not just your outfit but your entire day." />
+        <Feature title="Why Sure Kicks?" text="We're on a mission to provide the most stylish and comfortable shoes to our valued customers, all while ensuring the highest quality and unbeatable customer service! We believe that the right pair of shoes can transform not just your outfit but your entire day.." />
       </div>
       <div className='sk__wsk-heading'>
         <h1 className='gradient__text'>We stand by the quality of every pair of shoes we offer</h1>
